f920ffe80bde3da4e5524af27c0cdd101688e3fe
Mumo <miguemumo99@correo.ugr.es>
2019-11-11 23:08:02 +0100

    CivitasJuego Terminado (Practica3)

--- CivitasJava/src/juegoTexto/JuegoTexto.java
+++ CivitasJava/src/juegoTexto/JuegoTexto.java
@@ -8,6 +8,7 @@ package juegoTexto;
 import civitas.CivitasJuego;
 import civitas.Dado ;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 /**
  *
@@ -18,9 +19,23 @@ public class JuegoTexto {
     
 
     public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
         ArrayList<String>  nombres = new ArrayList<>();
-        nombres.add("Ivan");
-        nombres.add("Mumo");
+        
+        int num_jugadores=0;
+        System.out.println("Introduce el numero de jugadores(2-4)");
+
+        while (num_jugadores < 2 || num_jugadores > 4){
+            num_jugadores = teclado.nextInt();
+        }
+
+        System.out.println("Introduce los nombres de los jugadores");
+        teclado.nextLine();
+        for (int i=0; i<num_jugadores; i++){
+            String name = teclado.nextLine();
+            System.out.println("Jugador " + name + " introducido");
+            nombres.add(name);  
+        }
         
         Dado.getInstance().setDebug(true);
         
